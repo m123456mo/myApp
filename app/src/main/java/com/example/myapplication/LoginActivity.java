32625fff4bd5b1f72e231e29e52ce3301c89981e
m123456mo <skyy.ocean.sea@gmail.com>
2021-04-16 23:49:58 +0430

    add login Activity

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -4,19 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        // abcdef
-        // 123
-        // bb
-    }
-
-    public void a() {
-       // aaaa
+        setContentView(R.layout.activity_login);
     }
 }
